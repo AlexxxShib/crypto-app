4f1c7f745562be93365d557b93c7413e23d92f98
Aleksei Shibkov <alexxxshib@gmail.com>
2024-02-07 18:13:17 +0400

    injection into Worker

--- app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
@@ -3,12 +3,15 @@ package com.example.cryptoapp.di
 import android.app.Application
 import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.example.cryptoapp.presentation.CoinPriceListActivity
+import com.example.cryptoapp.presentation.CryptoApp
 import dagger.BindsInstance
 import dagger.Component
 
 @ApplicationScope
 @Component(modules = [DataModule::class, ViewModelModule::class])
 interface ApplicationComponent {
+
+    fun inject(application: CryptoApp)
 
     fun inject(activity: CoinPriceListActivity)
 
